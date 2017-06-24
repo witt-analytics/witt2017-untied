fde0566abdb40ae408dc40577b6abb3754eab52a
Auburngrads <auburngrads@live.com>
2017-06-24 10:12:00 -0400

    6/24 Capstone Update

--- clustering.R
+++ clustering.R
@@ -3,15 +3,16 @@
 #h2o.init()
 h2o.shutdown()
 
+Data <- readr::read_csv('dtr.csv')
+
 ##########
-head(dtr)
-str(dtr)
-summary(dtr)
+head(Data)
+str(Data)
+summary(Data)
 
-plot(dtr[c("s", "g")], main = "Sales X SKUs")
+plot(Data[,1:20], main = "Sales X SKUs")
 
-d <- scale(dtr)
-
+d <- Data[,c(1:20)]
 
 ##########
 wss <- (nrow(d)-1)*sum(apply(d,2,var))
@@ -25,18 +26,22 @@ wss
 
 ##########
 #hd <- as.h2o(d)
-c14 <- kmeans(d, centers = 14, nstart = 100, iter.max = 1000)
-c14
 
-c20 <- kmeans(d, centers = 20, nstart = 250, iter.max = 1000)
-c20
+Cluster <- kmeans(d, centers = 13, nstart = 100, iter.max = 1000)
+#Cluster <- kmeans(d, centers = 20, nstart = 250, iter.max = 1000)
+#Cluster <- kmeans(d, centers = 22, nstart = 250, iter.max = 1000)
+#Cluster <- kmeans(d, centers = 15, nstart = 100, iter.max = 1000)
 
-c22 <- kmeans(d, centers = 22, nstart = 250, iter.max = 1000)
-c22
+agg14 <- aggregate(d, by=list(c14$cluster), FUN = mean)
+str(agg14)
 
-c15 <- kmeans(d, centers = 15, nstart = 100, iter.max = 1000)
-c15
-
-aggregate(d, by=list(c15$cluster), FUN = mean)
+Data$Cluster <- Cluster$cluster
 #ddf <- as.data.frame(c15)
 #ddf <- data.frame(d,c15)
+
+
+library(nnet)
+
+mod <- multinom(Cluster ~ ., data = Data)
+pred <- predict(mod,Data,"probs")
+Pred <- round(pred, digits = 4)
